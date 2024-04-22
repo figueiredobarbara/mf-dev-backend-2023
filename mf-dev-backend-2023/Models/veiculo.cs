e828c880192e5d42346d004acfa6166beeabb4d4
Bárbara Figueiredo <barbaraflassis@gmail.com>
2024-04-22 20:55:15 -0300

    Configuração do Banco de Dados e da Tabela Veículo

---
+++ mf-dev-backend-2023/Models/veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        public int AnoModelo { get; set; }
+    }
+}
